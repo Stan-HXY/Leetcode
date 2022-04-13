1d9ada38bcc851bafd31e2237b9f590c80154401
Stan He <stanhe518@gmail.com>
2022-04-12 23:11:32 -0400

    master

--- leetcode/42. Trapping Rain Water.java
+++ leetcode/42. Trapping Rain Water.java
@@ -24,16 +24,27 @@ Output: 6
 
 class Solution {
     public int trap(int[] height) {
-        int l = 0, r = height.length - 1;
-
-
-
-
-
-
-
-
-
-        return 0;
+        int n = height.length;
+        if(n < 3) return 0;
+        int res = 0;
+        int l = 0, r = n - 1;
+        int maxL = 0, maxR = 0;
+        int i = maxL;
+        while(l <= r){
+            maxL = Math.max(maxL, height[l]);
+            maxR = Math.max(maxR, height[r]);
+            int curr_ht = Math.min(maxL, maxR);
+            int curr_res = curr_ht - height[i];
+            if(curr_res > 0){
+                res += curr_res;
+            }
+            if(maxL <= maxR){
+                i = ++l;
+            }
+            else{
+                i = ++r;
+            }
+        }
+        return res;
     }
 }
