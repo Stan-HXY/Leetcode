f2d9794b5bb00bebd44663632ecdb00b133a9953
Stan_Mac <stanhe518@gmail.com>
2022-05-03 19:48:21 -0400

    master

---
+++ leetcode/697. Degree of an Array.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+
+    // ============= one pass solution ================
+    public int findShortestSubArray(int[] nums) {
+        HashMap<int, int[]> M = new HashMap<>();
+        for(int i = 0; i < nums.length; ++i){
+            
+        }
+    }
+}
